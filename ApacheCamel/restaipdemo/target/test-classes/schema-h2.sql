2b5e65809e1716a156f0d0aa0eeb4db0000915f8
viplavan <74528093+viplavan@users.noreply.github.com>
2024-05-20 16:37:13 -0500

    Add files via upload
    Added target and other files

---
+++ ApacheCamel/restaipdemo/target/test-classes/schema-h2.sql
@@ -0,0 +1,7 @@
+create table cloud_vendor_info (
+    vendor_address varchar(255),
+    vendor_id varchar(255) not null,
+    vendor_name varchar(255),
+    vendor_phone_number varchar(255),
+    primary key (vendor_id)
+);
